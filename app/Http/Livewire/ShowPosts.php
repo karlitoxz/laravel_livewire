6c52355271ad39d60d058f5c9de9d2920c6ca5d4
karlitoxz <electronicaysistemas@gmail.com>
2023-03-06 18:16:38 -0500

    Traer datos y ponerlos en una tabla

--- app/Http/Livewire/ShowPosts.php
+++ app/Http/Livewire/ShowPosts.php
@@ -3,12 +3,14 @@
 namespace App\Http\Livewire;
 
 use Livewire\Component;
+use App\Models\Post;
 
 class ShowPosts extends Component
 {
 
     public function render()
     {
-        return view('livewire.show-posts');
+        $posts = Post::all();
+        return view('livewire.show-posts', compact('posts'));
     }
 }
