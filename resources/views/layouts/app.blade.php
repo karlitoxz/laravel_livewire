86e9c869e52085424172a8f7aeb47006f75935f1
karlitoxz <electronicaysistemas@gmail.com>
2023-03-06 17:09:48 -0500

    Preparar el layout de livewire

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -16,7 +16,7 @@
 
         <!-- Scripts -->
        <!-- @vite(['resources/css/app.css', 'resources/js/app.js'])-->
-       <!--<script src="{{ asset('js/app.js') }}" defer></script>-->
+       <script src="{{ asset('js/app.js') }}" defer></script>
 
     </head>
     <body class="font-sans antialiased">
