fcda0c0e0754a71a4a21247bf608e07794bbe8cf
karlitoxz <electronicaysistemas@gmail.com>
2023-03-06 19:07:49 -0500

    Crear post y guardar en la base de datos.

--- app/Http/Livewire/CreatePost.php
+++ app/Http/Livewire/CreatePost.php
@@ -2,14 +2,26 @@
 
 namespace App\Http\Livewire;
 
+
 use Livewire\Component;
+use App\Models\Post;
 
 class CreatePost extends Component
 {
     public $open = false;
+    public $title, $content;
 
     public function render()
     {
         return view('livewire.create-post');
+    }
+
+    public function save(){
+        Post::create([
+            'title' => $this->title,
+            'content' => $this->content
+        ]);
+        $this->open = false;
+        //https://www.youtube.com/watch?v=Wcf4pbnt5lI&list=PLZ2ovOgdI-kWqCet33O0WezN14KShkwER&index=7
     }
 }
--- resources/views/livewire/create-post.blade.php
+++ resources/views/livewire/create-post.blade.php
@@ -5,12 +5,30 @@
 
     <x-jet-dialog-modal wire:model='open'>
             <x-slot name="title">
+                Crear nuevo Post
             </x-slot>
 
             <x-slot name="content">
+                    <div class="mb-4">
+                        <x-jet-label value='Titulo del post' />
+                        <x-jet-input type='text' class="w-full" wire:model.defer="title" />
+                        {{$title}}
+                    </div>
+                    <div class="mb-4">
+                        <x-jet-label value='Contenido del post' />
+                        <x-jet-input type='text' class="w-full"  wire:model.defer="content" />
+                        {{$content}}
+                    </div>
             </x-slot>
 
             <x-slot name="footer">
+                <x-jet-secondary-button wire:click="$set('open', false)">
+                    Cancelar
+                </x-jet-secondary-button>
+
+                <x-jet-danger-button wire:click="save">
+                    Crear Post
+                </x-jet-danger-button>
             </x-slot>
 
     </x-jet-dialog-modal>
